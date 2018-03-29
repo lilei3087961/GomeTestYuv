1c147d88ce8dafb49480bd7bef0b1293bca21e0f
lilei <lei.li@gometech.com.cn>
2018-03-29 13:52:15 +0800

    适配不同项目，修改logtag

--- jni/gomeMultiScan.c
+++ jni/gomeMultiScan.c
@@ -63,7 +63,7 @@ int gome_dump_yuv(void *addr, int length,char* name)
 	//char image_name[20];
 	
 	//sprintf(image_name, JPG, name);
-	ALOGD("lilei libGomeMultiScan ##gome_dump_yuv 111");
+	ALOGD("libGomeMultiScan ##gome_dump_yuv 111");
 	if((fp = fopen(name, "w")) == NULL)
 	{
 		perror("gome_dump_yuv libGomeMultiScan Fail to fopen JPG");
@@ -76,24 +76,39 @@ int gome_dump_yuv(void *addr, int length,char* name)
 	return 0;
 }
 void getQrcodeOffset(int *x,int *y){
-	char MODEL_U9[] ="GOME 2018M29A";
-	char WECHAT[]="wechat";
-	char ALIPAY[]="alipay";
-	char OFO[]="ofo";
-	char MOBIKE[] = "mobike";
+	char MODEL_U9[] ="gm22a";
+	char MODEL_U7[]="gm12b";
+	char WECHAT[]="com.tencent.mm";
+	char ALIPAY[]="com.eg.android.AlipayGphone";
+	char OFO[]="so.ofo.labofo";
+	char MOBIKE[] = "com.mobike.mobikeapp";
 	char scanApp[PROPERTY_VALUE_MAX];
 	char model[PROPERTY_VALUE_MAX];
 	property_get("persist.sys.multiscan_app",scanApp,"null");
 	property_get("ro.product.model",model,"null");
 
 	if(0 == strcmp(model,MODEL_U9)){  //u9
-		if(0 == strcmp(scanApp,WECHAT)){ //微信300*300
+		if(0 == strcmp(scanApp,WECHAT)){ //微信864*480 300*300
 			*x = 0;
 			*y = 0;
-		}else if(0 == strcmp(scanApp,ALIPAY)){ //支付宝600*600
+		}else if(0 == strcmp(scanApp,ALIPAY)){ //支付宝1920*1080 600*600
 			*x = -30;
 			*y = 10;
-		}else if(0 == strcmp(scanApp,OFO)){  //ofo 400*400
+		}else if(0 == strcmp(scanApp,OFO)){  //ofo 1280*720 400*400
+			*x = -65;
+			*y = 0;
+		}else if(0 == strcmp(scanApp,MOBIKE)){ //mobike 1280*720 400*400
+			*x = 5;
+			*y = 0;
+		}
+	}else if(0 == strcmp(model,MODEL_U7)){  //u7
+		if(0 == strcmp(scanApp,WECHAT)){ //微信1280*640 400*400
+			*x = 0;
+			*y = 0;
+		}else if(0 == strcmp(scanApp,ALIPAY)){ //支付宝 1280*720 400*400
+			*x = -20;
+			*y = 0;
+		}else if(0 == strcmp(scanApp,OFO)){  //ofo 1280*720 400*400
 			*x = -65;
 			*y = 0;
 		}else if(0 == strcmp(scanApp,MOBIKE)){ //mobike 400*400
@@ -113,16 +128,16 @@ void getQrcodeOffset(int *x,int *y){
 		*y = testOffsetY;
 	}
 	//for test end
-	ALOGD("lilei libGomeMultiScan getQrcodeOffset() scanApp:%s,model:%s *x:%d,*y:%d,strcmp(scanApp,WECHAT):%d,testOffsetX:%d,testOffsetY:%d",scanApp,model,*x,*y,strcmp(scanApp,WECHAT),testOffsetX,testOffsetY);
+	ALOGD("libGomeMultiScan getQrcodeOffset() scanApp:%s,model:%s *x:%d,*y:%d,strcmp(scanApp,WECHAT):%d,testOffsetX:%d,testOffsetY:%d",scanApp,model,*x,*y,strcmp(scanApp,WECHAT),testOffsetX,testOffsetY);
 
 }
 //两个nv21图片合成
 void merge_two_yuv_for_nv21(unsigned char *big_buff,int width_big,int height_big,unsigned char * small_buff,int width_small,int height_small)
 {
-    ALOGD("lilei libGomeMultiScan merge_two_yuv_for_nv21 big_buff:%p,width_big:%d height_big:%d small_buff:%p width_small:%d height_small:%d \n",big_buff,width_big,height_big,small_buff,width_small,height_small);
+    ALOGD("libGomeMultiScan merge_two_yuv_for_nv21 big_buff:%p,width_big:%d height_big:%d small_buff:%p width_small:%d height_small:%d \n",big_buff,width_big,height_big,small_buff,width_small,height_small);
     if(width_big<width_small || height_big < height_small)
     {
-        ALOGD("lilei libGomeMultiScan merge_two_yuv_for_nv21 error width_big<width_small || height_big < height_small \n");
+        ALOGD("libGomeMultiScan merge_two_yuv_for_nv21 error width_big<width_small || height_big < height_small \n");
         return;
     }
     int small_vu_distance = width_small*height_small;
@@ -143,7 +158,7 @@ void merge_two_yuv_for_nv21(unsigned char *big_buff,int width_big,int height_big
     unsigned char * big_buff_vu = big_buff+large_vu_distance+vu_center_offset+vu_offset;
     unsigned char * small_buff_y = small_buff; 
     unsigned char * small_buff_vu = small_buff + small_vu_distance;
-    ALOGD("lilei libGomeMultiScan ##merge_two_yuv_for_nv21  large_vu_distance:%d,small_vu_distance:%d,offset_horizontal:%d,offset_vertical:%d \n",large_vu_distance,small_vu_distance,offset_horizontal,offset_vertical);
+    ALOGD("libGomeMultiScan ##merge_two_yuv_for_nv21  large_vu_distance:%d,small_vu_distance:%d,offset_horizontal:%d,offset_vertical:%d \n",large_vu_distance,small_vu_distance,offset_horizontal,offset_vertical);
     for(int i=0;i<height_small;i++)
     {  
         memcpy(big_buff_y,small_buff_y,width_small);
@@ -168,7 +183,7 @@ void gome_merge_jpeg2yuv_for_nv21(const char *jpegPath,int width_small,int heigh
         //buff1为jpeg分配内存
         mjpeg_buff = (unsigned char*)malloc(width_small * height_small * 2);
         if(mjpeg_buff ==  NULL) {
-                ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step1 mjpeg_buff malloc err\n");
+                ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step1 mjpeg_buff malloc err\n");
                 return;
         }else{
                 memset(mjpeg_buff, 0, width_small * height_small * 2);
@@ -176,20 +191,20 @@ void gome_merge_jpeg2yuv_for_nv21(const char *jpegPath,int width_small,int heigh
         //buff2为jpeg转码后的yuv422数据分配内存
         yuyv_buff = (unsigned char*)malloc(width_small * height_small * 2);
 	if(yuyv_buff ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step2 yuyv_buff malloc err\n");
+		ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step2 yuyv_buff malloc err\n");
 	}else{
 		memset(yuyv_buff, 0, WIDTH * HIGHT * 2);
 	}
         //为yuv422转换为nv21数据分配内存
         yuv420_buff = (unsigned char*)malloc(width_small * height_small * 1.5);
 	if(yuv420_buff ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step3 yuv420_buff malloc yuv420_buff err\n");
+		ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() step3 yuv420_buff malloc yuv420_buff err\n");
 	}else{
 		memset(yuv420_buff, 0, WIDTH * HIGHT * 1.5);
 	}
 
         FILE *pfile = fopen(jpegPath,"rb");
-        ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() 111 pfile:%p \n",pfile);
+        ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() 111 pfile:%p \n",pfile);
         fseek(pfile,0,SEEK_END); /* 定位到文件末尾 */
         int len= ftell(pfile);
         fseek(pfile,0,SEEK_SET); /* 定位到文件开头 */
@@ -197,16 +212,16 @@ void gome_merge_jpeg2yuv_for_nv21(const char *jpegPath,int width_small,int heigh
         jpeg_decode(&yuyv_buff, mjpeg_buff, &k, &j);   //将jpeg转码成yuyv422
         YUV422ToNv21(yuyv_buff,yuv420_buff,width_small,height_small); //将yuyv422转码成nv21  
         fclose(pfile); 
-        ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() 222 mjpeg_buff:%p len:%d  \n",mjpeg_buff,len);
+        ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_nv21() 222 mjpeg_buff:%p len:%d  \n",mjpeg_buff,len);
         merge_two_yuv_for_nv21(camera_buff,width_big,height_big,yuv420_buff,width_small,height_small);
 }
 //两个yv12图片合成
 void merge_two_yuv_for_yv12(unsigned char *big_buff,int width_big,int height_big,unsigned char * small_buff,int width_small,int height_small)
 {
-    ALOGD("lilei libGomeMultiScan merge_two_yuv_for_yv12 big_buff:%p,width_big:%d height_big:%d small_buff:%p width_small:%d height_small:%d \n",big_buff,width_big,height_big,small_buff,width_small,height_small);
+    ALOGD("libGomeMultiScan merge_two_yuv_for_yv12 big_buff:%p,width_big:%d height_big:%d small_buff:%p width_small:%d height_small:%d \n",big_buff,width_big,height_big,small_buff,width_small,height_small);
     if(width_big<width_small || height_big < height_small)
     {
-        ALOGD("lilei libGomeMultiScan merge_two_yuv_for_yv12 error width_big<width_small || height_big < height_small \n");
+        ALOGD("libGomeMultiScan merge_two_yuv_for_yv12 error width_big<width_small || height_big < height_small \n");
         return;
     }
     int small_vu_distance = width_small*height_small;
@@ -230,7 +245,7 @@ void merge_two_yuv_for_yv12(unsigned char *big_buff,int width_big,int height_big
     unsigned char * small_buff_y = small_buff;
     unsigned char * small_buff_v = small_buff + small_vu_distance;
     unsigned char * small_buff_u = small_buff + small_vu_distance*5/4;
-    ALOGD("lilei libGomeMultiScan ##merge_two_yuv_for_yv12  large_vu_distance:%d,small_vu_distance:%d,offset_horizontal:%d,offset_vertical:%d \n",large_vu_distance,small_vu_distance,offset_horizontal,offset_vertical);
+    ALOGD("libGomeMultiScan ##merge_two_yuv_for_yv12  large_vu_distance:%d,small_vu_distance:%d,offset_horizontal:%d,offset_vertical:%d \n",large_vu_distance,small_vu_distance,offset_horizontal,offset_vertical);
     for(int i=0;i<height_small;i++)
     {
         memcpy(big_buff_y,small_buff_y,width_small);
@@ -258,27 +273,27 @@ void gome_merge_jpeg2yuv_for_yv12(const char *jpegPath,int width_small,int heigh
         //buff1为jpeg分配内存
         mjpeg_buff = (unsigned char*)malloc(width_small * height_small * 2);
         if(mjpeg_buff ==  NULL) {
-                ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step1 mjpeg_buff malloc err\n");
+                ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step1 mjpeg_buff malloc err\n");
         }else{
                 memset(mjpeg_buff, 0, width_small * height_small * 2);
         }
         //buff2为jpeg转码后的yuv422数据分配内存
         yuyv_buff = (unsigned char*)malloc(width_small * height_small * 2);
 	if(yuyv_buff ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step2 yuyv_buff malloc err\n");
+		ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step2 yuyv_buff malloc err\n");
 	}else{
 		memset(yuyv_buff, 0, WIDTH * HIGHT * 2);
 	}
         //为yuv422转换为nv21数据分配内存
         yuv420_buff = (unsigned char*)malloc(width_small * height_small * 1.5);
 	if(yuv420_buff ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step3 yuv420_buff malloc yuv420_buff err\n");
+		ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() step3 yuv420_buff malloc yuv420_buff err\n");
 	}else{
 		memset(yuv420_buff, 0, WIDTH * HIGHT * 1.5);
 	}
 
         FILE *pfile = fopen(jpegPath,"rb");
-        ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() 111 pfile:%p \n",pfile);
+        ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() 111 pfile:%p \n",pfile);
         fseek(pfile,0,SEEK_END); /* 定位到文件末尾 */
         int len= ftell(pfile);
         fseek(pfile,0,SEEK_SET); /* 定位到文件开头 */
@@ -286,7 +301,7 @@ void gome_merge_jpeg2yuv_for_yv12(const char *jpegPath,int width_small,int heigh
         jpeg_decode(&yuyv_buff, mjpeg_buff, &k, &j);   //将jpeg转码成yuyv422
         YUV422ToYv12(yuyv_buff,yuv420_buff,width_small,height_small); //将yuyv422转码成yv12
         fclose(pfile);
-        ALOGD("lilei libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() 222 mjpeg_buff:%p len:%d  \n",mjpeg_buff,len);
+        ALOGD("libGomeMultiScan gome_merge_jpeg2yuv_for_yv12() 222 mjpeg_buff:%p len:%d  \n",mjpeg_buff,len);
         merge_two_yuv_for_yv12(camera_buff,width_big,height_big,yuv420_buff,width_small,height_small);
 }
 int get_camera_buff()
@@ -298,23 +313,23 @@ int get_camera_buff()
 		memset(yuv420_buff_big, 0, WIDTH_BIG * HIGHT_BIG * 1.5);
 	}       
         //////step2
-        ALOGD("lilei libGomeMultiScan get_camera_buff() 111");
+        ALOGD("libGomeMultiScan get_camera_buff() 111");
         char stryuvpath[80]={"/sdcard/DCIM/camera.yuv"};
         FILE *pyuvfile;
         if((pyuvfile = fopen(stryuvpath,"rb")) == NULL){
-	     ALOGD("lilei libGomeMultiScan open /sdcard/DCIM/camera.yuv error ");
+	     ALOGD("libGomeMultiScan open /sdcard/DCIM/camera.yuv error ");
 	     return 0;
 	} 
-	ALOGD("lilei libGomeMultiScan get_camera_buff() 222");
+	ALOGD("libGomeMultiScan get_camera_buff() 222");
 	fseek(pyuvfile,0,SEEK_END); /* 定位到文件末尾 */
-	ALOGD("lilei libGomeMultiScan get_camera_buff() 333");
+	ALOGD("libGomeMultiScan get_camera_buff() 333");
 	int lenyuv= ftell(pyuvfile);
-	ALOGD("lilei libGomeMultiScan get_camera_buff() 444");
+	ALOGD("libGomeMultiScan get_camera_buff() 444");
 	fseek(pyuvfile,0,SEEK_SET); /* 定位到文件开头 */
-	ALOGD("lilei libGomeMultiScan get_camera_buff() 555");
+	ALOGD("libGomeMultiScan get_camera_buff() 555");
 	size_t result = fread(yuv420_buff_big,1,lenyuv,pyuvfile);
 	fclose(pyuvfile);
-	ALOGD("lilei libGomeMultiScan get_camera_buff() 666 lenyuv:%d result:%d\n",lenyuv,result);
+	ALOGD("libGomeMultiScan get_camera_buff() 666 lenyuv:%d result:%d\n",lenyuv,result);
 	return 1;
 }
 //将nv21合并到yv12图像中
@@ -326,9 +341,9 @@ void gome_merge_nv21_to_yv12(const char *yuvImgPath,int width_small,int height_s
 	property_get("persist.sys.multiscan_app",scanApp,"null");
 	property_get("ro.product.model",model,"null");
     FILE *pfile = fopen(yuvImgPath,"rb");
-    ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12()** 111 pfile:%p scanApp:%s yuvImgPath:%s,yuvImgPath len:%d (NULL != pfile):%d\n",pfile,scanApp,yuvImgPath,strlen(yuvImgPath),(NULL != pfile));
+    ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12()** 111 pfile:%p scanApp:%s yuvImgPath:%s,yuvImgPath len:%d (NULL != pfile):%d\n",pfile,scanApp,yuvImgPath,strlen(yuvImgPath),(NULL != pfile));
     if(NULL == pfile){
-    	ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12()** 112 !! no fclose err cannot find image file:%s",yuvImgPath);
+    	ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12()** 112 !! no fclose err cannot find image file:%s",yuvImgPath);
     	//fclose(pfile);
     	return;
     }
@@ -337,14 +352,14 @@ void gome_merge_nv21_to_yv12(const char *yuvImgPath,int width_small,int height_s
 	//为从sdcard读取的nv21图片分配内存,防止同一个变量被两个方法调用
 	yuv420_buff = (unsigned char*)malloc(width_small * height_small * 1.5);
 	if(yuv420_buff ==  NULL){
-		printf("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 113 yuv420_buff malloc yuv420_buff err\n");
+		printf("libGomeMultiScan gome_merge_nv21_to_yv12() 113 yuv420_buff malloc yuv420_buff err\n");
 	}else{
 		memset(yuv420_buff, 0, width_small * height_small * 1.5);
 	}
 	//step1 为nv21转换为yv12数据分配内存
 	yuv420_buff_for_yv12 = (unsigned char*)malloc(width_small * height_small * 1.5);
 	if(yuv420_buff_for_yv12 ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 114 malloc yuv420_buff_for_yv12 err\n");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 114 malloc yuv420_buff_for_yv12 err\n");
 	}else{
 		memset(yuv420_buff_for_yv12, 0, width_small * height_small * 1.5);
 	}
@@ -357,21 +372,21 @@ void gome_merge_nv21_to_yv12(const char *yuvImgPath,int width_small,int height_s
 	//gome_dump_yuv(yuv420_buff_for_yv12,width_small*height_small*3/2,"out/qrd_image_yuv420.yuv");
 	Nv21ToYv12(yuv420_buff,yuv420_buff_for_yv12,width_small,height_small); //将nv21转码成yv12
 	fclose(pfile);
-	ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 222 yuv420_buff_for_yv12:%p len:%d  test 11.27\n",yuv420_buff_for_yv12,len);
+	ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 222 yuv420_buff_for_yv12:%p len:%d  test 11.27\n",yuv420_buff_for_yv12,len);
 	merge_two_yuv_for_yv12(camera_buff,width_big,height_big,yuv420_buff_for_yv12,width_small,height_small);
 
 	//step3 释放分配的内存
 	if(yuv420_buff_for_yv12 != NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 331 yuv420_buff_for_yv12 != NULL call free()");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 331 yuv420_buff_for_yv12 != NULL call free()");
 		free(yuv420_buff_for_yv12);
 	}else{
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 332 yuv420_buff_for_yv12 == NULL");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 332 yuv420_buff_for_yv12 == NULL");
 	}
 	if(yuv420_buff != NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 441 yuv420_buff != NULL call free()");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 441 yuv420_buff != NULL call free()");
 		free(yuv420_buff);
 	}else{
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_yv12() 442 yuv420_buff == NULL");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_yv12() 442 yuv420_buff == NULL");
 	}
 }
 //将nv21合并到nv21图像中
@@ -379,9 +394,9 @@ void gome_merge_nv21_to_nv21(const char *yuvImgPath,int width_small,int height_s
 {
 	FILE *pfile = fopen(yuvImgPath,"rb");
 
-	ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21() 111 pfile:%p NULL != pfile:%d\n",pfile,(NULL != pfile));
+	ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21() 111 pfile:%p NULL != pfile:%d\n",pfile,(NULL != pfile));
 	if(NULL == pfile){
-    	ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21()** 112 !! no fclose err cannot find image file:%s",yuvImgPath);
+    	ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21()** 112 !! no fclose err cannot find image file:%s",yuvImgPath);
     	//fclose(pfile);
     	return;
 	}
@@ -391,7 +406,7 @@ void gome_merge_nv21_to_nv21(const char *yuvImgPath,int width_small,int height_s
 	//step1为yuv422转换为nv21数据分配内存
 	yuv420_buff_for_nv21 = (unsigned char*)malloc(width_small * height_small * 1.5);
 	if(yuv420_buff_for_nv21 ==  NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21() 113  malloc yuv420_buff_for_nv21 err\n");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21() 113  malloc yuv420_buff_for_nv21 err\n");
 	}else{
 		memset(yuv420_buff_for_nv21, 0, width_small * height_small * 1.5);
 	}
@@ -404,29 +419,29 @@ void gome_merge_nv21_to_nv21(const char *yuvImgPath,int width_small,int height_s
 	//gome_dump_yuv(yuv420_buff_for_nv21,width_small*height_small*3/2,"/sdcard/DCIM/qrd_image_yuv420.yuv");
 	//YUV422ToYv12(yuyv_buff,yuv420_buff_for_nv21,width_small,height_small); //将yuyv422转码成yv12
 	fclose(pfile);
-	ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21() 222 yuv420_buff_for_nv21:%p len:%d  \n",yuv420_buff_for_nv21,len);
+	ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21() 222 yuv420_buff_for_nv21:%p len:%d  \n",yuv420_buff_for_nv21,len);
 	merge_two_yuv_for_nv21(camera_buff,width_big,height_big,yuv420_buff_for_nv21,width_small,height_small);
 
 	//step3 释放分配的内存
 	if(yuv420_buff_for_nv21 != NULL){
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21() 331 yuv420_buff_for_nv21 != NULL call free()");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21() 331 yuv420_buff_for_nv21 != NULL call free()");
 		free(yuv420_buff_for_nv21);
 	}else{
-		ALOGD("lilei libGomeMultiScan gome_merge_nv21_to_nv21() 332 yuv420_buff_for_nv21 == NULL");
+		ALOGD("libGomeMultiScan gome_merge_nv21_to_nv21() 332 yuv420_buff_for_nv21 == NULL");
 	}
 
 }
 void relaseMemory(){
 	if(yuv420_buff != NULL){
-		ALOGD("lilei relaseMemory() 11 yuv420_buff not null call free()");
+		ALOGD("libGomeMultiScan 11 yuv420_buff not null call free()");
 		free(yuv420_buff);
 	}else{
-		ALOGD("lilei relaseMemory() 222 yuv420_buff is null");
+		ALOGD("libGomeMultiScan 222 yuv420_buff is null");
 	}
 }
 void gome_testYuv()
 {
-	ALOGD("lilei libGomeMultiScan testyuv.c gome_testYuv() 000 \n");
+	ALOGD("libGomeMultiScan testyuv.c gome_testYuv() 000 \n");
         if(get_camera_buff() == 1){
         	gome_merge_jpeg2yuv_for_nv21("/sdcard/DCIM/1.jpg",320,240,yuv420_buff_big,WIDTH_BIG,HIGHT_BIG);
         }
